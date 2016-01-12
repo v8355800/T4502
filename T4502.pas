afb05766afb15f4e061cb55ec6c63c24c017cdf6
ASTRA <cRash85@mail.ru>
2016-01-12 16:22:22 +0300

    [Work] 1

--- T4502.pas
+++ T4502.pas
@@ -19,34 +19,34 @@ type
 
 
 //    procedure RsetT4502;  stdcall; external 'pci1751t4502.dll';
-    procedure U02(s:string);  stdcall; external 'pci1751t4502.dll';
-
-    procedure U11(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure U12(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure U21(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure U22(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure U31(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure U32(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure U41(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure U42(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure U51(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure U52(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure U61(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure U62(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure U71(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure U72(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure U73(s:string);  stdcall; external 'pci1751t4502.dll';
-
-    procedure D01(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure D02(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure Y02(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure Y01(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure R(s:string);  stdcall; external 'pci1751t4502.dll';
-    procedure KM(n:byte; s:string);stdcall; external 'pci1751t4502.dll';
-    procedure PlanPultPrin(var Ntst,Npt,Npl,RgP:word);  stdcall; external 'pci1751t4502.dll';
-    function T4502Ready(flag:boolean):boolean; stdcall; external 'pci1751t4502.dll';
-    procedure EndTst; stdcall; external 'pci1751t4502.dll';
-    procedure EndProg(GrKlas:word);stdcall;external 'pci1751t4502.dll';
+//    procedure U02(s:string);  stdcall; external 'pci1751t4502.dll';
+//
+//    procedure U11(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure U12(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure U21(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure U22(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure U31(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure U32(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure U41(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure U42(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure U51(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure U52(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure U61(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure U62(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure U71(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure U72(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure U73(s:string);  stdcall; external 'pci1751t4502.dll';
+//
+//    procedure D01(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure D02(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure Y02(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure Y01(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure R(s:string);  stdcall; external 'pci1751t4502.dll';
+//    procedure KM(n:byte; s:string);stdcall; external 'pci1751t4502.dll';
+//    procedure PlanPultPrin(var Ntst,Npt,Npl,RgP:word);  stdcall; external 'pci1751t4502.dll';
+//    function T4502Ready(flag:boolean):boolean; stdcall; external 'pci1751t4502.dll';
+//    procedure EndTst; stdcall; external 'pci1751t4502.dll';
+//    procedure EndProg(GrKlas:word);stdcall;external 'pci1751t4502.dll';
   end;
 
 type
@@ -75,10 +75,10 @@ end;
 
 { TT4502_Commands }
 
-procedure TT4502_Commands.K01;
-begin
-
-end;
+//procedure TT4502_Commands.K01;
+//begin
+//
+//end;
 
 end.
 
